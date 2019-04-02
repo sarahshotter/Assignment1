7f893cee38d1d58f3964dd34c351bda93a17234d
Sarah Shotter <c3211824@uon.edu.au>
2019-04-02 23:28:43 +0000

    added alphabet to functions

--- main.c
+++ main.c
@@ -11,23 +11,20 @@ char encrypt(char *x, char *y, char shift);
 char decrypt(char *x, char *y, char shiftBack);
 
 int main() {
-    char alpha[26], alphaCopy[26]; //Declaration of 2 arrays of length 26
     char k = 5, g = -5;
-    int i; 
     
-    for (i = 0; i < 26; i++) { //array with 26 elements
-        alpha[i] = i + 65; //elements range from 65 to 90
-        alphaCopy[i] = i + 65; //second array with elements ranging from 65 to 90
-        /* there are two arrays with the same elements to allow us to edit one from the 
-        other without changing the elements of the first one without the need for temporary variables*/
-    }
     
-    /*passing the elements of both the alpha and alphaCopy array to the encrypt function*/
-    encrypt(alpha, alphaCopy, k);
-    printf("%s\n", alphaCopy);
     
-    decrypt(alpha, alphaCopy, g);
-    printf("%s\n", alphaCopy);
+    char sentenceO[] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+    char sentenceE[] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+    
+    encrypt(sentenceO, sentenceE, k);
+    printf("%s", sentenceE);
+    
+//    char sentenceOriginal[] = {THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG}
+//    char sentenceEnc[] = sentenceOriginal[];
+//    encrypt(sentenceOriginal, sentenceEnc, k);
+//    printf("%s", sentenceEnc);
     
     return 0;
 }
@@ -35,8 +32,16 @@ int main() {
 /*Definition of the encrypt function occurs after the int main() code block*/
 
 char encrypt(char *x, char *y, char shift) {
-    printf("Enter a shift value:    ");
-    scanf("%c", &shift);
+    
+    char alpha[26], alphaCopy[26];
+    int i;
+    
+    for (i = 0; i < 26; i++) { //array with 26 elements
+        alpha[i] = i + 65; //elements range from 65 to 90
+        alphaCopy[i] = i + 65; //second array with elements ranging from 65 to 90
+        /* there are two arrays with the same elements to allow us to edit one from the 
+        other without changing the elements of the first one without the need for temporary variables*/
+    }
     
     int indexT, index; //indexT represents a temporary index, and index that of the elements in the arrays
     
@@ -49,30 +54,38 @@ char encrypt(char *x, char *y, char shift) {
     /*the for loop takes values of x[] i + some shift's value and assigns it to the array y[]'s ith element and then repeats this 
     process for all required values of index to suffice the conditions of the for loop thus encrypting the y[] array*/
     for (index = 0; index < 26 - tempShift; index++) {  
-        y[index] = x[index + tempShift];
+        y[index] = alpha[index + tempShift];
     }
     
     /*The for loop takes the values shift factor distance from the end of y[] + the indexT and assigns the value found in the indexT 
     positions of x[]. This allows us to circumnavigate the issue of accessing data from outside the array when having to rotate at the 
     end of the alphabet back to the start.*/
     for (indexT = 0; indexT < tempShift; indexT++) {
-        y[26 - tempShift + indexT] = x[indexT];
+        alphaCopy[26 - tempShift + indexT] = alpha[indexT];
     }
     return 0;
 }
 
 char decrypt(char *x, char *y, char shiftBack) {
 	
+	char alpha[26], alphaCopy[26];
+	int i;
+	
+	for (i = 0; i < 26; i++) { //array with 26 elements
+        alpha[i] = i + 65; //elements range from 65 to 90
+        alphaCopy[i] = i + 65; //second array with elements ranging from 65 to 90
+        /* there are two arrays with the same elements to allow us to edit one from the 
+        other without changing the elements of the first one without the need for temporary variables*/
+    }
+	
 	int ind, tempIndex; //the indices for the arrays and the temporary indices
 	
 	char tempShiftBack = abs((26 + shiftBack)%26);
-	
-	for (tempIndex = 0; tempIndex < tempShiftBack; tempIndex++) {
-		x[tempIndex] = y[26 - tempShiftBack + tempIndex];
-	}
-	
 	for (ind = 0; ind < 26 - tempShiftBack; ind++) {
-		x[ind + tempShiftBack] = y[ind];
+		alpha[ind + tempShiftBack] = alphaCopy[ind];
+	}
+	for (tempIndex = 0; tempIndex < tempShiftBack; tempIndex++) {
+		alpha[tempIndex] = alphaCopy[26 - tempShiftBack + tempIndex];
 	}
 	return 0;
 }
