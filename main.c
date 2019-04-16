27d5e0de67bc06b7344f78e127f84b442f4da57e
Sarah <c3211824@uon.edu.au>
2019-04-17 07:48:50 +1000

    Update main.c
    Working substitution encryption

--- main.c
+++ main.c
@@ -13,21 +13,20 @@ int main() {
     FILE *input;
     
     int choice;
-    printf("Choose which function to perform:   \n");
+    printf("Choose which function to perform:   \n"); //prints the options to the screen
     printf("1: Rotation Encryption\n");
     printf("2: Rotation Decryption\n");
     printf("3: Substitution Encryption\n");
     printf("4: Substitution Decryption\n");
-    scanf("%d", &choice);
+    scanf("%d", &choice); //saves the choice of the user to the variable then continues to switch case to carry out the function
     
-    
-    switch (choice) {
+    switch (choice) { //case 1 carries out the shift function
         case 1: fp = fopen("/Users/Sarebear/Desktop/Cipher/Message.txt", "r"); //reads the file from this location
             fgets(message, 1000, fp); //takes the file up to 1000 characters and stores it in the array message which was declared earlier
             printf("The original message is:    %s\n", message);
             shift(message);
             printf("The altered message is: %s\n", message); //prints out the encrypted message
-            break;
+            break; //case 2 carries out the reset function
         case 2: input = fopen("/Users/Sarebear/Desktop/Cipher/shiftFour.txt", "r");
             fgets(message, 1000, input);
             printf("The original message is:    %s\n", message);
@@ -42,6 +41,31 @@ int main() {
             break;
     }
     
+    char alpha[26];
+    char beta[26];
+    char subsMessage[1000];
+    FILE *fin;
+    fin = fopen("/Users/Sarebear/Desktop/Cipher/SubstitutionEncryption.txt", "r");
+    fgets(subsMessage, 1000, fin);
+    printf("Original message is:    %s\n", subsMessage);
+    
+    for (int i = 0; i < 26; i++) {
+        alpha[i] = i + 65;
+    }
+    
+    for (int n = 0; n < 26; n++) {
+        beta[n] = alpha[25 - n];
+    }
+    
+    printf("Standard alphabet:                   %s\n", alpha);
+    printf("Alphabet with letter substitution:   %s\n", beta);
+    int k;
+    for (k = 0; k < sizeof(subsMessage); k++) {
+        if (subsMessage[k] != ' '){
+            subsMessage[k] = beta[subsMessage[k] - 65];
+        }
+    }
+    printf("Substituted message is: %s\n", subsMessage);
 }
 
 void shift(char *info) { //function shift takes an array and adds a shift factor to each of the individual elements
@@ -91,3 +115,32 @@ void reset(char *codedInfo) {
         }
     }
 }
+
+/* SUBSTITUTION
+ create an array that contains 26 letters of the alphabet
+ we want to make a second array that randomly calls upon the elements of the first array and distributes them into the second
+ ok
+ so
+ 
+ let alphabet be an array with 26 elements from i = 0, to i = 25
+ each element i, is equal to its element number + 65
+ this makes it equal to its ASCII value
+ 
+ This will give us an array alpha[26];
+ 
+ let there be a second array beta[26] with elements ranging from n = 0, through to n = 25
+ s.t. beta[n] = alpha[rand() % (25 - n)]
+ 
+ char alpha[26];
+ char beta[26];
+ for (i = 0; i < 26; i++) {
+ alpha[i] = i + 65;
+ }
+ 
+ created a new alphabet from the old one
+ essentially a lookup table that the message will be drafted from
+ 
+ if the message and message length is hardcoded into the programme it mostly works
+ if the sizeof(subsMessage) is the max distance of k then it works brilliantly
+ 
+ */
