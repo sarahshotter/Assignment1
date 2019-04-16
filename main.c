47b5ead275c284f2ba3b840680ca14ed1b0bca70
Sarah <c3211824@uon.edu.au>
2019-04-16 15:45:49 +1000

    Added a shift function
    added shift function which will act as my encrypt function.
    DONT CALL IT ENCRYPT or the computer has a fucking spaz

--- main.c
+++ main.c
@@ -3,29 +3,37 @@
 #include <string.h>
 #include <strings.h>
 
+void shift(char *info); //this is the prototype of the encryption function
+
 int main() {
-    int n; //can call on n as many times as you want inside for loops
-    int shift; //this is the value that is going to shift the alphabet by. It has not been initialised as the user is to enter the value
     char message[1000]; //this sets the maximum number of characters in the message to be 1000 so as not to overload the computer
-    
-    
-    printf("Enter a value to shift alphabet by:    \n"); //prints to console
-    scanf("%d", &shift); //this reads what is input by the user and assigns it to the variable shift
     
     FILE *input; //declares a variable of type "file"
     input = fopen("/Users/Sarebear/Desktop/Cipher/Message.txt", "r"); //reads the file from this location
     fgets(message, 1000, input); //takes the file up to 1000 characters and stores it in the array message which was declared earlier
     
-    for (n = 0; message[n] != '\0'; n++) { //for each value between 0 and until there are no more characters in message, n will increment
-        if (message[n] != ' ') { //whilever the character in the position of n is not a <space>
-            message[n] = message[n] + shift; //the character in position n of message is now equal to its value + shift
-        }
-    }
-    /*The above for loop is stable and shifts each character of the message, but not the spaces. howvere, it does not recirculate at Z back to A*/
-    for (n = 0; message[n] != '\0'; n++) {
-        if (message[n] > 'Z') { //whenever the value is greater than 'Z' (i.e. ASCII > 90) the program should restart back at A
-            message[n] = message[n] - 26; //to restart at A simply subtract 26 from value returned
-        }
-    }
-    printf("The altered message is: %s\n", message);
+    shift(message); //calls shift function to encrypt message
+    printf("The altered message is: %s\n", message); //prints out the encrypted message
 }
+
+void shift(char *info) { //function shift takes an array and adds a shift factor to each of the individual elements
+    int n, shiftFactor;
+    printf("Enter a value to shift the message by:  \n");
+    scanf("%d", &shiftFactor);
+    if (shiftFactor < 0) { //if the shift factor is negative, the equivalent rotation can be achieved by taking the remainder of 26 + that shift factor
+        shiftFactor = (shiftFactor + 26)%26;
+        printf("To allow for the rotational nature of the alphabet, shift has been altered to:  %d\n", shiftFactor);
+    }
+    
+    for (n = 0; info[n] != '\0'; n++){ //as long as the element is not a space or an empty element at the end of the array the function will add the shift factor onto the element's value even if it exceeds the value of Z (i.e. 90 in ASCII)
+        if (info[n] != ' ') {
+            info[n] = info[n] + shiftFactor;
+        }
+    }
+    for (n = 0; info[n] != '\0'; n++) { //the function will then process each of the elements and those that are over the value of 'Z' (i.e. > 90) will have 26 subtracted from them to return them to the start of the alphabet and begin the cycle again thus retaining the cycling nature of a rotational cipher
+        if (info[n] > 'Z') {
+            info[n] = info[n] - 26;
+        }
+    }
+}
+
