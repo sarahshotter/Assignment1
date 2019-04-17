7f91dda2b8f5c2fa57fb99bf459954181b5424f6
Sarah <c3211824@uon.edu.au>
2019-04-17 11:01:20 +1000

    Update main.c
    substitution encryption function
    lowercase alphabet accounted for
    punctuation accounted for
    files written in properly
    commenting added

--- main.c
+++ main.c
@@ -7,10 +7,18 @@ void shift(char *info); //this is the prototype of the encryption function
 
 void reset(char *codedInfo);
 
+void subs(char *content);
+
+void subsDec(char *codedContent);
+
 int main() {
-    char message[1000];
-    FILE *fp;
-    FILE *input;
+    char message[1000]; //a maximum message length of 1000 characters was set to ensure that the memory of the processor was not compromised by running the programme
+    
+    //the corresponding declarations for files for each of the functions lies below with the format being "f<function_number>"
+    FILE *fone;
+    FILE *ftwo;
+    FILE *fthree;
+    FILE *ffour;
     
     int choice;
     printf("Choose which function to perform:   \n"); //prints the options to the screen
@@ -20,52 +28,49 @@ int main() {
     printf("4: Substitution Decryption\n");
     scanf("%d", &choice); //saves the choice of the user to the variable then continues to switch case to carry out the function
     
-    switch (choice) { //case 1 carries out the shift function
-        case 1: fp = fopen("/Users/Sarebear/Desktop/Cipher/Message.txt", "r"); //reads the file from this location
-            fgets(message, 1000, fp); //takes the file up to 1000 characters and stores it in the array message which was declared earlier
+    switch (choice) {
+            
+            /*Case 1: carries out the shift function. It is a rotational cipher encryption. It reads from a file and allows the user to input the key, then printing the rotated message to the console*/
+            
+        case 1: fone = fopen("/Users/Sarebear/Desktop/Cipher/Message.txt", "r"); //reads the file from this location
+            fgets(message, 1000, fone); //takes the file up to 1000 characters and stores it in the array message which was declared earlier
             printf("The original message is:    %s\n", message);
             shift(message);
-            printf("The altered message is: %s\n", message); //prints out the encrypted message
+            printf("The altered message is:     %s\n", message); //prints out the encrypted message
             break; //case 2 carries out the reset function
-        case 2: input = fopen("/Users/Sarebear/Desktop/Cipher/shiftFour.txt", "r");
-            fgets(message, 1000, input);
+            
+            /*Case 2: carries out the reset function. It is a rotational cipher decryption. It reads from a file and allows the user to input a key to decrypt by. At this point it can only decrypt by brute force attack. It prints the unencrypted message to the console*/
+            
+        case 2: ftwo = fopen("/Users/Sarebear/Desktop/Cipher/RotationDecrypt.txt", "r");
+            fgets(message, 1000, ftwo);
             printf("The original message is:    %s\n", message);
             reset(message);
-            printf("The reset message is:   %s\n", message);
+            printf("The reset message is:       %s\n", message);
             break;
-        case 3://calls on substitution encryption function (unwritten)
+            
+            /*Case 3: carries out the subs function. It is a substitutional cipher encryption. It reads from a file based on the computers main frame. Prints the alphabet in order and then prints a substituted alphabet which is used as a lookup table. Prints the substituted message*/
+            
+        case 3: fthree = fopen("/Users/Sarebear/Desktop/Cipher/SubstitutionEncryption.txt", "r");
+            fgets(message, 1000, fthree);
+            printf("Original message is:    %s\n", message);
+            subs(message);
+            printf("Substituted message is: %s\n", message);
             break;
-        case 4://calls on substitution decryption function (unwritten)
+            
+            /*Case 4: carries out subsDec function. It is a substitutional cipher decryption. */
+            
+        case 4: ffour = fopen("/Users/Sarebear/Desktop/Cipher/SubstitutionDecryption.txt", "r");
+            fgets(message, 1000, ffour);
+            printf("Original encrypted message is:  %s\n", message);
+            //                    subsDec(message);
+            printf("Decrypted message is:           %s\n", message);
             break;
-        default: printf("Try again, but this time choose an option that is actually there\n");
+            
+        default: printf("Try again, but this time choose an option that is actually there\n"); //the user has input something other than 1, 2, 3, or 4 as they have either misunderstood the instructions or something has gone wrong. This allows them to try again.
             break;
     }
     
-    char alpha[26];
-    char beta[26];
-    char subsMessage[1000];
-    FILE *fin;
-    fin = fopen("/Users/Sarebear/Desktop/Cipher/SubstitutionEncryption.txt", "r");
-    fgets(subsMessage, 1000, fin);
-    printf("Original message is:    %s\n", subsMessage);
     
-    for (int i = 0; i < 26; i++) {
-        alpha[i] = i + 65;
-    }
-    
-    for (int n = 0; n < 26; n++) {
-        beta[n] = alpha[25 - n];
-    }
-    
-    printf("Standard alphabet:                   %s\n", alpha);
-    printf("Alphabet with letter substitution:   %s\n", beta);
-    int k;
-    for (k = 0; k < sizeof(subsMessage); k++) {
-        if (subsMessage[k] != ' '){
-            subsMessage[k] = beta[subsMessage[k] - 65];
-        }
-    }
-    printf("Substituted message is: %s\n", subsMessage);
 }
 
 void shift(char *info) { //function shift takes an array and adds a shift factor to each of the individual elements
@@ -80,16 +85,24 @@ void shift(char *info) { //function shift takes an array and adds a shift factor
     }
     
     for (n = 0; info[n] != '\0'; n++){ //as long as the element is not a space or an empty element at the end of the array the function will add the shift factor onto the element's value even if it exceeds the value of Z (i.e. 90 in ASCII)
-        if (info[n] != ' ') {
+        
+        if ((info[n] >= 65) && (info[n] <= 90)) { //the ASCII values of A and Z respectively
             info[n] = info[n] + shiftFactor;
+            if (info[n] > 90) {         //the function will then process each of the elements and those that are over the value of 'Z' (i.e.                         > 90) will have 26 subtracted from them to return them to the start of the alphabet and begin the                            cycle again thus retaining the cycling nature of a rotational cipher
+                info[n] = info[n] - 26;
+            }
+            
+        } else if ((info[n] >= 97) && (info[n] <= 122)) { //these are the parameters for the lowercase alphabet
+            info[n] = info[n] - 32 + shiftFactor; //this will shift the lower case values into the upper case ascii values
+            if (info[n] > 90) {
+                info[n] = info[n] - 26; //returns values of letters back A and starts reading through the alphabet again.
+            }
+            
+        } else {
+            info[n] = info[n]; //to accommodate for punctuation, it is dealt with separately and its ascii values lie outside of the aparmeters specified above. We don't want to shift it as it would make the message more difficult to read.
         }
     }
     
-    for (n = 0; info[n] != '\0'; n++) { //the function will then process each of the elements and those that are over the value of 'Z' (i.e. > 90) will have 26 subtracted from them to return them to the start of the alphabet and begin the cycle again thus retaining the cycling nature of a rotational cipher
-        if (info[n] > 'Z') {
-            info[n] = info[n] - 26;
-        }
-    }
 }
 
 void reset(char *codedInfo) {
@@ -104,43 +117,62 @@ void reset(char *codedInfo) {
     }
     
     for (n = 0; codedInfo[n] != '\0'; n++) {
-        if (codedInfo[n] != ' ') {
+        
+        if ((codedInfo[n] >= 65) && (codedInfo[n] <= 90)) { //values between 65 and 90 give the capitalised letters of the alphabet
             codedInfo[n] = codedInfo[n] - resetFactor; //essentially doing the opposite of when we encrypted the message
+            if (codedInfo[n] < 'A') {
+                codedInfo[n] = codedInfo[n] + 26;
+            }
+            
+        } else if ((codedInfo[n] >= 97) && (codedInfo[n] <= 122)) { //these parameters for the lowercase alphabet set up the elseif statement to change all lowercase letters to their corresponding uppercase letters and then decrypt them
+            codedInfo[n] = codedInfo[n] - 32 - resetFactor; //subtracting 32 will change their ASCII value to, e.g. a - 32 = A
+            if (codedInfo[n] < 'A') {
+                codedInfo[n] = codedInfo[n] + 26; //this will return it back into the domain of A to Z as the rotation cipher is cyclic
+            }
+            
+        } else {
+            codedInfo[n] = codedInfo[n]; //this accommodates for any other symbols used in the message, i.e. any punctuation, so that it will remain a constant through out and not be shifted with the other letters
         }
     }
     
-    for (n = 0; codedInfo[n] != '\0'; n++) {
-        if ((codedInfo[n] < 'A') && (codedInfo[n] != ' ')) { //as <space> has a lower value in ASCII need to ensure that the <space> elements aren't also shifted just because they have a value less than 'A' (i.e. < 65)
-            codedInfo[n] = codedInfo[n] + 26; //increasing by 26 will return all values back into the alphabet
+}
+
+void subs(char *content) {
+    char alpha[26];
+    char beta[26];
+    int i, n, k;
+    
+    for (i = 0; i < 26; i++) {
+        alpha[i] = i + 65;
+    }
+    
+    for (n = 0; n < 26; n++) {
+        beta[n] = alpha[25 - n];
+    }
+    
+    printf("Standard alphabet:                   %s\n", alpha);
+    printf("Alphabet with letter substitution:   %s\n", beta);
+    
+    for (k = 0; k < sizeof(content); k++) {
+        if (content[k] != ' '){
+            content[k] = beta[content[k] - 65];
         }
     }
 }
 
-/* SUBSTITUTION
- create an array that contains 26 letters of the alphabet
- we want to make a second array that randomly calls upon the elements of the first array and distributes them into the second
- ok
- so
+//void subsDec(char *codedContent) {
+//
+//}
+
+/* TODO
+ rotation decryption
+ sort through and pick out the most common letters in the message -  they are most likely t/e
+ most common three letter word is the/and
  
- let alphabet be an array with 26 elements from i = 0, to i = 25
- each element i, is equal to its element number + 65
- this makes it equal to its ASCII value
+ substitution decryption
+ download a dictionary - there should be one built in to a c library
+ most common three letter words are the/and
+ most common letters in English are t, e, a
  
- This will give us an array alpha[26];
- 
- let there be a second array beta[26] with elements ranging from n = 0, through to n = 25
- s.t. beta[n] = alpha[rand() % (25 - n)]
- 
- char alpha[26];
- char beta[26];
- for (i = 0; i < 26; i++) {
- alpha[i] = i + 65;
- }
- 
- created a new alphabet from the old one
- essentially a lookup table that the message will be drafted from
- 
- if the message and message length is hardcoded into the programme it mostly works
- if the sizeof(subsMessage) is the max distance of k then it works brilliantly
  
  */
